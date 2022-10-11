2d4ddc10f1d499251a93a9aea5d2e22e0a997044
André Fonseca Borges <borges.an@hotmail.com>
2022-10-11 06:41:44 -0300

    Up Class

--- Arquivos_para_dar_aula/2.0_POO(Classes)/1.0_Classses/main.py
+++ Arquivos_para_dar_aula/2.0_POO(Classes)/1.0_Classses/main.py
@@ -1,7 +1,7 @@
 from pessoas import Pessoa
 
 p1 = Pessoa('Carlos', 31)
-p1.comer('maça')
-p1.parar_comer()
-p1.parar_comer()
-p1.comer('Pera')
+p2 = Pessoa('Adriely', 19)
+
+print(p1.get_ano_nascimento())
+print(p2.get_ano_nascimento())
