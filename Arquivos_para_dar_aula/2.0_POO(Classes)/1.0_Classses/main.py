aacac828f5eeff5bf57073ac6926ec1d48dacbb0
André Borges <borges.an@hotmail.com>
2023-05-22 07:09:16 -0300

    up Code

--- Arquivos_para_dar_aula/2.0_POO(Classes)/1.0_Classses/main.py
+++ Arquivos_para_dar_aula/2.0_POO(Classes)/1.0_Classses/main.py
@@ -3,5 +3,11 @@ from pessoas import Pessoa
 p1 = Pessoa('Carlos', 31)
 p2 = Pessoa('Adriely', 19)
 
+p1.falar('POO')
+p2.falar('Novelas')
+p2.comer('Maçã')
+p2.falar('filmes')
+p1.falar('Banco de dados')
+
 print(p1.get_ano_nascimento())
 print(p2.get_ano_nascimento())
