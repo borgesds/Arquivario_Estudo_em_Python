4908dc5ad17ad54b077bc11696b912863236fc36
André Borges <borges.an@hotmail.com>
2023-06-22 19:46:39 -0300

    up Code

--- Arquivos_para_dar_aula/3.0_componentes/2.0_OS/encontra.py
+++ Arquivos_para_dar_aula/3.0_componentes/2.0_OS/encontra.py
@@ -1,32 +1,34 @@
 import os
+from convert import formata_tamanho
 
 
-caminho_procura = '/home/borges/Downloads'
-termo_procura = 'train.csv'
+def encontrar_arquivo(caminho_procura, termo_procura):
 
-conta = 0
+    conta = 0
 
-for raiz, diretorio, arquivos in os.walk(caminho_procura):
-    for arquivo in arquivos:
-        if termo_procura in arquivo:
-            try:
-                conta += 1
-                caminho_completo = os.path.join(raiz, arquivo)
-                nome_arquivo, ext_arquivo = os.path.splitext(arquivo)
-                tamanho = os.path.getsize(caminho_completo)
+    for raiz, diretorio, arquivos in os.walk(caminho_procura):
+        for arquivo in arquivos:
+            if termo_procura in arquivo:
+                try:
+                    conta += 1
 
-                print()
-                print('Encontrei o arquivo:', arquivo)
-                print('Caminho:', caminho_completo)
-                print('Nome:', nome_arquivo)
-                print('Extensão:', ext_arquivo)
-                print('Tamanho:', tamanho)
-            except PermissionError as e:
-                print('Sem permissões, error:', e)
-            except FileNotFoundError as e:
-                print('Arquivo não encontrado, error:', e)
-            except Exception as e:
-                print('Error desconhecido, error:', e)
+                    caminho_completo = os.path.join(raiz, arquivo)
+                    nome_arquivo, ext_arquivo = os.path.splitext(arquivo)
+                    tamanho = os.path.getsize(caminho_completo)
 
-print()
-print(f'{conta} arquivo(s) encontrado(s).')
+                    print()
+                    print('Encontrei o arquivo:', arquivo)
+                    print('Caminho:', caminho_completo)
+                    print('Nome:', nome_arquivo)
+                    print('Extensão:', ext_arquivo)
+                    print('Tamanho:', tamanho)
+                    print('Tamanho formatado:', formata_tamanho(tamanho))
+                except PermissionError as e:
+                    print('Sem permissões, error:', e)
+                except FileNotFoundError as e:
+                    print('Arquivo não encontrado, error:', e)
+                except Exception as e:
+                    print('Error desconhecido, error:', e)
+
+    print()
+    print(f'{conta} arquivo(s) encontrado(s).')
---
+++ Arquivos_para_dar_aula/3.0_componentes/2.0_OS/main.py
@@ -0,0 +1,8 @@
+from encontra import encontrar_arquivo
+
+
+caminho_procura = input('Digite um caminho: ')
+
+termo_procura = input('Digite o arquivo: ')
+
+encontrar_arquivo(caminho_procura, termo_procura)
