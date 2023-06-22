bc10365f1c4bd0a0589289499c8cea98979cfe95
André Borges <borges.an@hotmail.com>
2023-06-22 06:50:21 -0300

    up Code

--- Arquivos_para_dar_aula/3.0_componentes/1.0_detetime/main.py
+++ Arquivos_para_dar_aula/3.0_componentes/1.0_detetime/main.py
@@ -1,4 +1,6 @@
 from datetime import datetime, timedelta
+from locale import setlocale, LC_ALL
+from calendar import mdays
 
 data = datetime(2023, 5, 8, 15, 25, 8)
 print(data.strftime('%d/%m/%Y %H:%M:%S'))
@@ -13,3 +15,15 @@ dif = data4 - data2
 
 print(data.strftime('%d/%m/%Y %H:%M:%S'))
 print(dif)
+
+# formatar
+setlocale(LC_ALL, 'pt_BR.utf-8')
+
+dt = datetime.now()
+mes_atual = int(dt.strftime('%m'))
+formatacao1 = dt.strftime('%A, %d de %B de %Y')
+formatacao2 = data.strftime('%d/%m/%Y %H:%M:%S')
+
+print(formatacao1)
+print(formatacao2)
+print(mes_atual, mdays[mes_atual])
