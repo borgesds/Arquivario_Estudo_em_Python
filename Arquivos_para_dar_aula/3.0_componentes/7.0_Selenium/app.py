a69495b0254e2b43c1c06ffcabbb3fe70c16541f
André Borges <borges.an@hotmail.com>
2023-06-28 07:11:59 -0300

    up Code

--- Arquivos_para_dar_aula/3.0_componentes/7.0_Selenium/app.py
+++ Arquivos_para_dar_aula/3.0_componentes/7.0_Selenium/app.py
@@ -7,21 +7,63 @@ https://chromedriver.chromium.org/downloads
 from selenium import webdriver
 from time import sleep
 
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-class CookieClicker:
+
+class ChromeAuto:
     def __init__(self):
-        self.SITE_LINK = 'https://www.google.com.br/'
+        # self.SITE_LINK = 'https://www.google.com.br/'
         options = webdriver.ChromeOptions()
         options.add_argument('user-data-dir=Perfil')
-        self.drive = webdriver.Chrome(options=options)
-        self.drive.maximize_window()
+        self.chrome = webdriver.Chrome(options=options)
+        self.chrome.maximize_window()
 
-    def abrir_site(self):
-        sleep(2)
-        self.drive.get(self.SITE_LINK)
-        sleep(10)
+    def clica_sing_in(self):
+        try:
+            btn_sign_in = self.chrome.find_element(
+                'css selector', 'a[data-ga-click="(Logged out) Header, clicked Sign in, text:sign-in"]'
+            )
+            btn_sign_in.click()
+        except Exception as e:
+            print('Erro ao clicar no Sing in:', e)
+
+    def acessa(self, site):
+        self.chrome.get(site)
+
+    def sair(self):
+        self.chrome.quit()
+
+    def fazer_login(self):
+        try:
+            # Definindo o tempo máximo de espera em segundos
+            wait = WebDriverWait(self.chrome, 10)
+
+            input_login = wait.until(EC.presence_of_element_located((
+                                     By.ID, 'login_field'
+                                     )))
+            input_password = wait.until(EC.presence_of_element_located((
+                                        By.ID, 'password'
+                                        )))
+            btn_login = wait.until(EC.presence_of_element_located((
+                                   By.NAME, 'commit'
+                                   )))
+
+            input_login.send_keys('borges.an@hotmail.com')
+            input_password.send_keys('xxxxxxxxxxx')
+            sleep(3)
+            btn_login.click()
+        except Exception as e:
+            print('LoginError:', e)
 
 
 if __name__ == '__main__':
-    teste = CookieClicker()
-    teste.abrir_site()
+    chrome = ChromeAuto()
+    chrome.acessa('https://github.com/')
+
+    chrome.clica_sing_in()
+    chrome.fazer_login()
+
+    sleep(30)
+    chrome.sair()
