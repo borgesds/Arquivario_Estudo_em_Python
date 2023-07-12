060f5e7f953c22a1d9aaf895357c64d8fe92c12f
André Borges <borges.an@hotmail.com>
2023-07-12 06:56:48 -0300

    up Code

--- Arquivos_para_dar_aula/4.0_DB/1.0_CRUD/mysql.py
+++ Arquivos_para_dar_aula/4.0_DB/1.0_CRUD/mysql.py
@@ -23,6 +23,53 @@ def conecta():
 
 with conecta() as conexao:
     with conexao.cursor() as cursor:
+        sql = 'INSERT INTO tbl_dados_c (ade, status, valor_x, situacao_x) VALUES'\
+              '(%s, %s, %s, %s)'
+
+        valor = (81111111, 'TESTE', '5555555', 'TESTE-')
+
+        cursor.execute(sql, valor)
+        conexao.commit()
+
+
+with conecta() as conexao:
+    with conexao.cursor() as cursor:
+        sql = 'INSERT INTO tbl_dados_c (ade, status, valor_x, situacao_x) VALUES'\
+              '(%s, %s, %s, %s)'
+
+        dados = [
+            (81111112, 'TESTE', '84555', 'TESTE-'),
+            (81111113, 'TESTE', '53355', 'TESTE-'),
+            (81111114, 'TESTE', '11555', 'TESTE-'),
+            (81111115, 'TESTE', '955', 'TESTE-'),
+                 ]
+
+        cursor.executemany(sql, dados)
+        conexao.commit()
+
+
+with conecta() as conexao:
+    with conexao.cursor() as cursor:
+        sql = 'DELETE FROM tbl_dados_c WHERE ade IN (%s, %s, %s)'
+
+        dados = (81111112, 81111113, 81111114)
+
+        cursor.execute(sql, dados)
+        conexao.commit()
+
+
+with conecta() as conexao:
+    with conexao.cursor() as cursor:
+        sql = 'UPDATE tbl_dados_c SET valor_x=%s WHERE ade=%s'
+
+        dados = (1000, 81111111)
+
+        cursor.execute(sql, dados)
+        conexao.commit()
+
+
+with conecta() as conexao:
+    with conexao.cursor() as cursor:
         cursor.execute("""SELECT ade, status, situacao_x
                           FROM tbl_dados_c
                           ORDER BY ade DESC
